5a6aec569e3141f0e94ef1f781bcc0d19c58bfec
Jesse Alenyika <alenyikaj@gmail.com>
2024-03-21 21:03:57 +0100

    Updated

--- Azure/0-local.tf
+++ Azure/0-local.tf
@@ -2,6 +2,6 @@ locals {
   env                 = "dev"
   region              = "eastus2"
   resource_group_name = "capstone"
-  eks_name            = "capstone-eks"
+  eks_name            = "eks"
   eks_version         = "1.27"
 }
