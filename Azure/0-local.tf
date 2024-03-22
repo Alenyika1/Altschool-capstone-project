42072791567f87aa7e9d90b03847cecdcd31a572
Alenyika Jesse <alenyikaj@gmail.com>
2024-03-22 14:08:47 +0100

    Delete Azure directory

--- Azure/0-local.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  env                 = "dev"
-  region              = "eastus2"
-  resource_group_name = "capstone"
-  eks_name            = "eks"
-  eks_version         = "1.27"
-}
--- Azure/2-resource-grp.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "capstone" {
-  name     = local.resource_group_name
-  location = local.region
-}
